83701351975d03e8c0f12ed637beda731d636f63
ysknsid25 <kengo071225@gmail.com>
2025-04-10 18:00:43 +0900

    init firebase
    Signed-off-by: ysknsid25 <kengo071225@gmail.com>

--- astro.config.ts
+++ astro.config.ts
@@ -21,7 +21,7 @@ import { pluginLineNumbers } from '@expressive-code/plugin-line-numbers'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  site: 'https://astro-erudite.vercel.app',
+  site: 'https://blog-e0dea.web.app',
   integrations: [
     expressiveCode({
       themes: ['github-light', 'github-dark'],
